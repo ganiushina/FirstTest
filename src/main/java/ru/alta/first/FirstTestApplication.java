b5e5bc5b650869f06b078edfaaaa0d0936414b49
ganiushina <32090008+ganiushina@users.noreply.github.com>
2022-01-19 13:32:22 +0400

    Initial commit

---
+++ src/main/java/ru/alta/first/FirstTestApplication.java
@@ -0,0 +1,13 @@
+package ru.alta.first;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/alta/first/FirstTestApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.alta.first;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
